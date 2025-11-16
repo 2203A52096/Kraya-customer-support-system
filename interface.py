452afc8fb2c041f5d79ed79a9a00233b5ccad3a6
Kothapelly Keerthana <141894913+2203A52096@users.noreply.github.com>
2025-11-16 13:27:40 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -461,7 +461,7 @@ So spill the beans, the weirder your description, the more fun our buddy adventu
         st.markdown(solution_html, unsafe_allow_html=True)
 
 # ---------------- MAIN UI ---------------- #
-def show_ui(food_model, food_vectorizer, fabric_model, fabric_vectorizer, electronics_data):
+def show_ui(food_model, food_vectorizer, fabric_model, electronics_data):
     import streamlit as st
     from sentence_transformers import SentenceTransformer
 
